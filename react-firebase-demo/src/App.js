758c3d437b133dc09752a09f4ee33f7c22ce5b38
n.pop <n.pop@accenture.com>
2017-04-10 17:04:26 +0300

    added the authentication to the project

--- react-firebase-demo/src/App.js
+++ react-firebase-demo/src/App.js
@@ -7,11 +7,18 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      speed: 1
+      speed: 0,
+      email:'',
+      password:''
     }
   }
 
   componentDidMount() {
+    this.loadDataBaseListener()
+    this.realTimeUserListener()
+  }
+
+  loadDataBaseListener = () => {
     const rootRef = firebase.database().ref().child('react')
     const speedRef = rootRef.child('speed')
     speedRef.on('value', snap => {
@@ -21,18 +28,66 @@ class App extends React.Component {
     })
   }
 
-  handleClick = () => {
-    this.setState({
-      speed: this.state.speed + 1
+  realTimeUserListener = () => {
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+      if(firebaseUser) {
+        console.log('you are now loggend in')
+        console.log(firebaseUser)
+      } else {
+        console.log('you are not loggen in')
+      }
     })
+  }
+
+  logIn = () => {
+    const email = this.state.email
+    const pass = this.state.password
+    const auth = firebase.auth()
+    const promise = auth.signInWithEmailAndPassword(email, pass)
+    promise.catch(e => console.log(e.message))
+  }
+
+  signUp = () => {
+    const email = this.state.email
+    const pass = this.state.password
+    const auth = firebase.auth()
+    const promise = auth.createUserWithEmailAndPassword(email, pass)
+    promise.catch(e => console.log(e.message))
+  }
+
+  logOut = () => {
+   firebase.auth().signOut()
+  }
+
+  handleInputChange = (event) => {
+    let id = event.target.id
+    if( id === 'email' ) {
+      this.setState({
+        email:event.target.value
+      })
+    }
+
+    if(id === 'password') {
+       this.setState({
+        password:event.target.value
+      })
+    }
   }
 
   render () {
     return(
       <div>
         <h1>React Firebase App</h1>
-        <button onClick={this.handleClick}>Apasa</button>
+        <button onClick={this.logIn}>Log In</button>
+        <button onClick={this.signUp}>Sign Up</button>
+        <button onClick={this.logOut}>Log out</button>
         <p>Counter Value: {this.state.speed}</p>
+        <div className="login_area">
+          <label htmlFor="email">Email:</label>
+          <input id="email" type="text" onChange={this.handleInputChange} />
+          <label htmlFor="password">Password:</label>
+          <input id="password" type="password" onChange={this.handleInputChange} />
+        </div>
       </div>
     )
   }
